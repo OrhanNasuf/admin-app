cbc3b055ea3463812f3276601c3f2f487a2cef83
orhan.nasufovski <orhan.nasufovski@virtualobject.se>
2018-07-06 14:42:16 +0200

    Add initial components.

---
+++ src/components/Test/StatelessCompTest.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StatelessCompTest = () => {
+    
+    return (
+        <div>
+            <h1>Test Component</h1>
+            <p>This Component is a stateless function rather than a class.</p>
+        </div>
+    );
+}
+
+export default StatelessCompTest;
