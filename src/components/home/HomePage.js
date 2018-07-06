50467d8ff90a2e9895a454ac8eda63c1c6abadda
orhan.nasufovski <orhan.nasufovski@virtualobject.se>
2018-07-06 16:50:52 +0200

    Add link to Test page.

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -9,6 +9,7 @@ class HomePage extends React.Component {
                 <h1>Pluralsight Administration</h1>
                 <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
                 <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+                <Link to="test" className="btn btn-primary btn-lg">Test</Link>
             </div>
         );
     }
