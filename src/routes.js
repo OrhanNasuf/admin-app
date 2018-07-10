9e705439fc7c067670861f3038eb25f1256c10ba
orhan.nasufovski <orhan.nasufovski@virtualobject.se>
2018-07-10 10:35:59 +0200

    Restore HomePage route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
-// import HomePage from './components/home/HomePage';
+import HomePage from './components/home/HomePage';
 import CustomPage from './components/custom/CustomPage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
@@ -9,8 +9,8 @@ import StatelessCompTest from './components/Test/StatelessCompTest';
 
 export default (
     <Route path="/" component={App}>
-        {/* <IndexRoute component={HomePage} /> */}
-        <IndexRoute component={CustomPage} />
+        <IndexRoute component={HomePage} />
+        {/* <IndexRoute component={CustomPage} /> */}
         <Route path="about" component={AboutPage} />
         <Route path="courses" component={CoursesPage} />
         <Route path="test" component={StatelessCompTest} />
