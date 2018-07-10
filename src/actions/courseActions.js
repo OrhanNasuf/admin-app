4a84f53322ba5c961d015d50b526c33f1ecfd76d
orhan.nasufovski <orhan.nasufovski@virtualobject.se>
2018-07-10 14:24:30 +0200

    Add faux server API

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,20 @@
-export function createCourse(course) {
+import * as type from './actionTypes';
+import courseApi from '../api/mockCourseApi';
+
+export function loadCoursesSuccess(courses) {
     return {
-        type: "CREATE_COURSE",
-        course: course
+        type: type.LOAD_COURSES_SUCCESS,
+        courses: courses
     };
+}
+
+export function loadCourses() {
+    return (dispatch) => {
+        return courseApi.getAllCourses().then((courses) => {
+            dispatch(loadCoursesSuccess(courses));
+
+        }).catch((error) => {
+            throw error;
+        });
+    }
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,12 +1,11 @@
+import * as type from '../actions/actionTypes';
+
 export default function courseReducer(state = [], action) {
 
     switch (action.type) {
 
-        case "CREATE_COURSE":
-            return [
-                ...state,
-                Object.assign({}, action.course)
-            ];
+        case type.LOAD_COURSES_SUCCESS:
+            return action.courses;
     
         default:
             return state;
