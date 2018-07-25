d2df0966a824174923c5f041ea8adfbd41b742cb
orhan.nasufovski <orhan.nasufovski@virtualobject.se>
2018-07-25 13:56:13 +0200

    add loading circle

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
 import {connect} from 'react-redux';
+import {CircularProgress} from 'material-ui';
+import {MuiThemeProvider} from 'material-ui/styles';
 
 class App extends React.Component {
 
@@ -13,6 +15,9 @@ class App extends React.Component {
                     loading={this.props.loading}
                 />
                 {this.props.children}
+                <MuiThemeProvider>
+                    {this.props.loading ? <CircularProgress size={200} thickness={10} style={{position: "absolute", left: "calc(50% - 100px)", top: "calc(50% - 100px)", zIndex: 5}}/> : null}
+                </MuiThemeProvider>
             </div>
         );
     }
